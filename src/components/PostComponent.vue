62be9262ebc95c64785cda224c4f689b236caf4f
Bruno-Felix <balvesfelix@gmail.com>
2024-05-18 17:24:38 -0300

    Apagando set inicial vue e criando component PostComponent

---
+++ src/components/PostComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="post-component">
+        <h2>Postagem</h2>
+
+        <div>Titulo: {{ title }}</div>
+        <div>Descrição: {{ description }}</div>
+        <div>Link: {{ link }}</div>
+    </div>
+</template>
+
+<script>
+import { ref, defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        title: String,
+        description: String,
+        link: String
+    },
+});
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/ForumPage.vue
+++ src/pages/ForumPage.vue
@@ -1,13 +1,36 @@
-<script setup>
-
-</script>
-
 <template>
-  <div>
-    ForumPage
-   
-  </div>
+  <section>
+    <h1>Forum Page</h1>
+
+    <div v-for="postagem in postagens" :key="postagem.id">
+      <PostComponent :title="postagem.title" :description="postagem.description" :link="postagem.link"/>
+    </div>
+  </section>
 </template>
+
+<script>
+import { ref, defineComponent } from 'vue';
+import PostComponent from '../components/PostComponent.vue';
+
+export default defineComponent({
+  data() {
+    return {
+      postagens: [
+        {
+          id: '1',
+          title: 'aaaaaaaa',
+          description: 'aaaaaaaaa',
+          link: 'aaaaaaaaaaaa'
+        }
+      ]
+    };
+  },
+
+  components: {
+    PostComponent
+  }
+});
+</script>
 
 <style scoped>
 
