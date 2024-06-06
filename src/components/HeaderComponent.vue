47b5e42bc849af13ee92709ac1028d5516214a83
Bruno-Felix <balvesfelix@gmail.com>
2024-06-06 18:36:16 -0300

    Criando componente Header e Footer

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,44 +1,48 @@
-<!-- <template> 
-  <div class="header-component">
+<template> 
+  <header>
+    <section>
       <h1><\UnbConnect></h1>
-        <ul>
-            <li>Fórum</li>
-            <li>Início</li>
-            <li>Nosso Impacto</li>
-            <li>Serviços</li>
-            <li>Quem Somos</li>
-          </ul>
 
-      <div>Titulo: {{ title }}</div>
-      <div>Descrição: {{ description }}</div>
-      <div>Link: {{ link }}</div>
-  </div>
+      <nav>
+        <ul>
+          <li>Início</li>
+          <li>Nosso Impacto</li>
+          <li>Serviços</li>
+          <li>Quem Somos</li>
+        </ul>
+      </nav>
+    </section>
+  </header>
 </template>
 
-
 <script>
-
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  props: {
-      title: String,
-      description: String,
-      link: String
-  },
-});
 </script>
 
-
-
-
 <style scoped> 
-  .header-component {
-      margin-left: 235px;
-      margin-right: 235px;
-      background-color: #B6D9B2;
-      border-radius: 10px;
+  header {
+  /* cabeçalho */
+  background-color: #008137;
+  padding: 11px 0; 
+  color: #FFFFFF;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+}
 
-  }
+nav {
+  display: flex;
+  margin-right: 250px;
+  justify-content: end; /* Alinha os itens do nav para a direita */
+  align-items: center; /* Centraliza verticalmente os itens */
+}
 
-</style> -->
+ul {
+  display: flex;
+  flex-direction: row;
+  gap: 50px; /* Ajusta a separação entre cada item da barra de navegação */
+  list-style-type: none; /* Remove os marcadores de lista padrão */
+  margin: 0;
+  padding: 0;
+}
+
+</style> 
--- src/pages/ForumPage.vue
+++ src/pages/ForumPage.vue
@@ -1,53 +1,25 @@
   
 <template>
-  <html>
     <section>
-      <!-- <HeaderComponent/> -->
-      <header>
-      <section>
-      <h1><\UnbConnect></h1>
-      <nav>
-        <ul>
-          <li>Início</li>
-          <li>Nosso Impacto</li>
-          <li>Serviços</li>
-          <li>Quem Somos</li>
-        </ul>
-    </nav>
-      </section>
-    </header>
+      <HeaderComponent/>
+      
+      <div v-for="postagem in postagens" :key="postagem.id">
+        <PostComponent 
+          :title="postagem.title" 
+          :description="postagem.description" 
+          :link="postagem.link" 
+          :imgSrc="postagem.imgSrc"/>
+      </div>
 
-     <body>
-      <!--
-            <div v-for="postagem in postagens" :key="postagem.id">
-                  <PostComponent :title="postagem.title" :description="postagem.description" :link="postagem.link"/>
-                  
-      </div> -->
-
-
-            <div v-for="postagem in postagens" :key="postagem.id">
-            <PostComponent 
-            :title="postagem.title" 
-            :description="postagem.description" 
-            :link="postagem.link" 
-            :imgSrc="postagem.imgSrc"/>
-            </div>
-            
-        
-          </body>
+      <FooterComponet/>
     </section>
-    <footer>
-      <p>©2024, UnBConnect.</p>
-      <p>Amarelo Girassol - Desenvolvimento de Software, 2024/1.</p>
-
-    </footer>
-  </html>
 </template>
-    
 
 <script>
 import { ref, defineComponent } from 'vue';
 import PostComponent from '../components/PostComponent.vue';
+import HeaderComponent from '../components/HeaderComponent.vue';
+import FooterComponet from '../components/FooterComponent.vue';
 
 export default defineComponent({
   data() {
@@ -109,7 +81,8 @@ export default defineComponent({
 
   components: {
     PostComponent,
-    // HeaderComponent
+    HeaderComponent,
+    FooterComponet
   }
 });
 
@@ -135,62 +108,5 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
-*  {
-  /* defini todas as tags de "espaçamentos" */
-  padding: 5;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-body {
-  /* corpo */
-background-color: #FFFFFF;
-font-family: 'Roboto', 'Arial', sans-serif;
-
-}
-
-header {
-  /* cabeçalho */
-  background-color: #008137;
-  padding: 11px 0; 
-  color: #FFFFFF;
-  width: 100%;
-  bottom: 0;
-  left: 0;
-}
-
-nav {
-  display: flex;
-  margin-right: 250px;
-  justify-content: end; /* Alinha os itens do nav para a direita */
-  align-items: center; /* Centraliza verticalmente os itens */
-}
-
-ul {
-  display: flex;
-  flex-direction: row;
-  gap: 50px; /* Ajusta a separação entre cada item da barra de navegação */
-  list-style-type: none; /* Remove os marcadores de lista padrão */
-  margin: 0;
-  padding: 0;
-}
-
-
-footer {
-  /* rodapé */
-  background-color: #12017D;
-  padding: 26px 0;
-  margin-top: 20px;
-  text-align: center;
-  color:#FFFFFF;
-  position: relative;
-  width: 100%;
-  top: 57px;
-  left: 0;
-
-  
-}
-
 
 </style>
