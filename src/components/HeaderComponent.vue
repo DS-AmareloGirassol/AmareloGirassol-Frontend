f1f23e0e266c7674310b855758ff9e79f125f0df
Bruno-Felix <balvesfelix@gmail.com>
2024-06-14 21:21:46 -0300

    Refatorando base Header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,19 @@
 <template> 
   <header>
-    <section>
-      <h1><\UnbConnect></h1>
+    <h1 class="header-logo"><\UnbConnect></h1>
 
-      <nav>
-        <ul>
-          <li>Início</li>
-          <li>Nosso Impacto</li>
-          <li>Serviços</li>
-          <li>Quem Somos</li>
-        </ul>
-      </nav>
-    </section>
+    <nav class="header-nav">
+      <ul>
+        <li class="header-nav-li">Meu Fluxo</li>
+        <li class="header-nav-li">Disciplinas</li>
+        <li class="header-nav-li">Fórum</li>
+      </ul>
+    </nav>
+
+    <div class="header-account">
+      <img class="header-account-view-img" src="../assets/account.png">
+      <img class="header-logout-img" src="../assets/logout.png">
+    </div>
   </header>
 </template>
 
@@ -19,19 +21,22 @@
 </script>
 
 <style scoped> 
-  header {
-  /* cabeçalho */
+header {
   background-color: #008137;
-  padding: 11px 0; 
+  padding: 10px 50px;
   color: #FFFFFF;
-  width: 100%;
   bottom: 0;
   left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
-nav {
+.header-logo{
+}
+
+.header-nav {
   display: flex;
-  margin-right: 250px;
   justify-content: end; /* Alinha os itens do nav para a direita */
   align-items: center; /* Centraliza verticalmente os itens */
 }
@@ -43,6 +48,29 @@ ul {
   list-style-type: none; /* Remove os marcadores de lista padrão */
   margin: 0;
   padding: 0;
+}
+
+.header-nav-li {
+  padding: 10px;
+  font-size: 20px;
+}
+
+.header-account {
+  height: 60px;
+}
+
+.header-account-view-img {
+  height: 40px;
+  margin-right: 30px;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.header-logout-img {
+  height: 40px;
+  cursor: pointer;
+  padding: 10px;
+  cursor: pointer;
 }
 
 </style> 
